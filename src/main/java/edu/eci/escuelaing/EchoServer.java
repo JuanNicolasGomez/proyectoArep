7a54451a603b1662471733b1e19de556ee4b0553
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-01-29 11:24:11 -0500

    Ejercicio 4.3.2

--- src/main/java/edu/eci/escuelaing/EchoServer.java
+++ src/main/java/edu/eci/escuelaing/EchoServer.java
@@ -4,6 +4,9 @@ package edu.eci.escuelaing;
 import java.io.*;
 
 public class EchoServer {
+
+    private static String operation = "cos";
+    private static boolean operationChanged = false;
     public static void main(String[] args) throws IOException {
         System.out.println("Servidor ejecutandose...");
         ServerSocket serverSocket = null;
@@ -27,7 +30,9 @@ public class EchoServer {
         String inputLine, outputLine;
         while ((inputLine = in.readLine()) != null) {
             System.out.println("Mensaje:"+inputLine);
-            outputLine = "Respuesta"+ Math.pow(Double.parseDouble(inputLine),2);
+
+            //outputLine = "Respuesta"+ Math.pow(Double.parseDouble(inputLine),2);
+            outputLine = "Respuesta"+ serverAnswer(inputLine);
             out.println(outputLine);
             if (outputLine.equals("Respuestas: Bye."))
                 break;
@@ -36,5 +41,37 @@ public class EchoServer {
         in.close();
         clientSocket.close();
         serverSocket.close();
+    }
+
+    public static double serverAnswer(String inputLine){
+        double ans = 0;
+        if (inputLine.length()>4) {
+            //System.out.println(inputLine.substring(0, 4));
+            if (inputLine.substring(0,4).equals("fun:")){
+                operation = inputLine.substring(4);
+                operationChanged = true;
+                //System.out.println(operation);
+            }
+        }
+        if (operationChanged == true){
+            operationChanged = false;
+        }else{
+            if(operation.equals("cos")){
+                //System.out.println("FUNCTION CHANGED TO COS");
+                ans = Math.cos(Double.parseDouble(inputLine));
+
+            }else if(operation.equals("sin")){
+                //System.out.println("FUNCTION CHANGED TO SIN");
+                ans = Math.sin(Double.parseDouble(inputLine));
+
+            }else if(operation.equals("tan")){
+                //System.out.println("FUNCTION CHANGED TO TAN");
+                ans = Math.tan(Double.parseDouble(inputLine));
+            }
+        }
+
+        return ans;
+
+
     }
 }
